5b23b3373a8d836b0b1003866f7815c9c2a7255a
Samuel Lima <samuel.lima@voffice.com.br>
2022-03-29 22:48:17 -0300

    modulo spring

---
+++ src/main/java/br/com/fiap/livraria/LivrariaFiapApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.livraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaFiapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaFiapApplication.class, args);
+	}
+
+}
